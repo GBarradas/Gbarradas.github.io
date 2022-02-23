f825cbbd32d1782e2ad0b112b28361aad7396b65
Gonçalo <gbarradas1@hotmail.com>
2022-02-23 18:19:51 +0000

    20220223-18.19

---
+++ UE/EI/js/info.js
@@ -0,0 +1,356 @@
+
+const  mat="Matemática"
+const inf="Informática"
+//1º Semestre
+let ipm={
+    semestre: 1 ,
+    codUC: "INF13186L" ,
+    nome: `Interação Pessoa-Máquina` ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota:15,
+},
+p1={
+    semestre: 1 ,
+    codUC: "INF13175L" ,
+    nome: "Programação I" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota:14,
+}
+sd={
+    semestre: 1 ,
+    codUC: "INF13177L" ,
+    nome: "Sistemas Digitais" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota:13,
+},
+alga={
+    semestre: 1 ,
+    codUC: "MAT0900L" ,
+    nome: "Álgebra Linear e Geometria Analítica" ,
+    aC:mat,
+    etcs: 6,
+    feita: false ,
+    nota:"-",
+},
+am1={
+    semestre: 1 ,
+    codUC: "MAT12877L" ,
+    nome: "Análise Matemática I" ,
+    aC:mat,
+    etcs: 6,
+    feita: true ,
+    nota:10,
+},
+//2º semestre
+ac1={
+    semestre: 2 ,
+    codUC: "INF13178L" ,
+    nome: "Arquitectura de Computadores I" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 18,
+},
+eda1={
+    semestre: 2 ,
+    codUC: "INF13188L" ,
+    nome: "Estrutura de Dados e Algoritmos I" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 16,
+},
+fg={
+    semestre: 2 ,
+    codUC: "FIS13008L" ,
+    nome: "Fisica-Geral I" ,
+    aC: "Fisica",
+    etcs: 6,
+    feita: true ,
+    nota: 14,
+},
+am2={
+    semestre: 2 ,
+    codUC: "MAT12878L" ,
+    nome: "Análise Matemática II" ,
+    aC:mat,
+    etcs: 6,
+    feita: false ,
+    nota:"-",
+},
+ipe={
+    semestre: 2 ,
+    codUC: "MAT12619L" ,
+    nome: "Intrudução à Probabilidade e Estatísca" ,
+    aC:mat,
+    etcs: 6,
+    feita: true ,
+    nota:12,
+},
+//3º Semestre
+ac2={
+    semestre: 3 ,
+    codUC: "INF13189L" ,
+    nome: "Arquitectura de Computadores II" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 16,
+},
+bd={
+    semestre: 3 ,
+    codUC: "INF13190L" ,
+    nome: "Base de Dados" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 13
+},
+cg={
+    semestre: 3 ,
+    codUC: "INF13192L" ,
+    nome: "Computação Gráfica" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 16,
+},
+p2={
+    semestre: 3 ,
+    codUC: "INF13194L" ,
+    nome: "Programação II" ,
+    aC:inf,
+    etcs: 6,
+    feita: true ,
+    nota: 19,
+},
+md={
+    semestre: 3 ,
+    codUC: "MAT0932L" ,
+    nome: "Matemática Discreta" ,
+    aC:mat,
+    etcs: 6,
+    feita: true ,
+    nota: 14,
+},
+//4º Semestre
+alp={
+    semestre: 4 ,
+    codUC: "INF13195L" ,
+    nome: "Autómatos e Linguagens de Programação" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+eda2={
+    semestre: 4 ,
+    codUC: "INF13196L" ,
+    nome: "Estrutura de Dados e Algoritmos II" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+ii={
+    semestre: 4 ,
+    codUC: "INF13199L" ,
+    nome: "Intrudução à Investigação" ,
+    aC:inf,
+    etcs: 3,
+    feita: false ,
+    nota: "-",
+},
+lc={
+    semestre: 4 ,
+    codUC: "INF13200L" ,
+    nome: "Lógica e Computação" ,
+    aC:inf,
+    etcs: 3,
+    feita: false ,
+    nota: "-",
+},
+rc={
+    semestre: 4 ,
+    codUC: "INF13201L" ,
+    nome: "Rede de Computadores" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+so={
+    semestre: 4 ,
+    codUC: "INF13202L" ,
+    nome: "Sistemas Operativos" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+//5º Semestre
+aa={
+    semestre: 5 ,
+    codUC: "INF13203L" ,
+    nome: "Apredizagem Automática" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+mds={
+    semestre: 5 ,
+    codUC: "INF13204L" ,
+    nome: "Metodologias e Desenvolvimento de Software" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+p3={
+    semestre: 5 ,
+    codUC: "INF13205L" ,
+    nome: "Programação III" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+sdi={
+    semestre: 5 ,
+    codUC: "INF13206L" ,
+    nome: "Sistemas Distribuidos" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+tw={
+    semestre: 5 ,
+    codUC: "INF13207L" ,
+    nome: "Tecnologias Web" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+//6º Semestre
+ia={
+    semestre: 6 ,
+    codUC: "INF13208L" ,
+    nome: "Inteligência Artifial" ,
+    aC:inf,
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+ep={
+    semestre: 6 ,
+    codUC: "INF13212L" ,
+    nome: "Estágio / Projeto" ,
+    aC:inf,
+    etcs: 12,
+    feita: false ,
+    nota: "-",
+},
+ei={
+    semestre: 6 ,
+    codUC: "GES2310L" ,
+    nome: "Empreendorismo e Inovação" ,
+    aC:"Gestão",
+    etcs: 6,
+    feita: false ,
+    nota: "-",
+},
+si={
+    semestre: 6 ,
+    codUC: "INF13210L" ,
+    nome: "Seguraça Informática" ,
+    aC:inf,
+    etcs: 3,
+    feita: false ,
+    nota: "-",
+},
+sma={
+    semestre: 6 ,
+    codUC: "INF13211L" ,
+    nome: "Sistemas Móveis e Aplicações" ,
+    aC:inf,
+    etcs: 3,
+    feita: false ,
+    nota: "-",
+}
+const s1={
+    nome: "1º",
+    numCadeiras:5,
+    cadeiras : [ipm, p1, sd, alga,am1],
+}
+const s2={
+    nome: "2º",
+    numCadeiras:5,
+    cadeiras : [ac1, eda1,fg, am2, ipe],
+}
+const s3={
+    nome: "3º",
+    numCadeiras:5,
+    cadeiras : [ac2,bd,cg,p2,md],
+}
+const s4={
+    nome: "4º",
+    numCadeiras:6,
+    cadeiras : [alp,eda2,ii,lc,rc,so],
+}
+const s5={
+    nome: "5º",
+    numCadeiras:5,
+    cadeiras : [aa,mds,p3,sdi,tw],
+}
+const s6={
+    nome: "6º",
+    numCadeiras:5,
+    cadeiras : [ia,ep,ei,si,sma],
+}
+const geral=[s1,s2,s3,s4,s5,s6]
+
+const apro = [];
+const cc1 =[]
+const cc2 =[]
+const cc3 =[]
+const cc4 =[]
+const cc5 =[]
+const cc6 =[]
+for(let i of geral){
+    for(let k of i.cadeiras){
+        if(k.feita){
+            apro.push(k);
+        }
+        switch(k.semestre){
+            case 1:
+                cc1.push(k);
+                break;
+            case 2:
+                cc2.push(k);
+                break;
+            case 3:
+                cc3.push(k);
+                break;
+            case 4:
+                cc4.push(k);
+                break;
+            case 5:
+                cc5.push(k);
+                break;
+            case 6:
+                cc6.push(k);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ UE/EI/js/table.js
@@ -0,0 +1,164 @@
+const table=[];
+function getInfo(){
+    for(let i of geral){
+        for(let k of i.cadeiras){
+            table.push(k)
+        }
+    }
+
+}
+function teste(){
+    initTable();
+    let terminal=document.getElementById("terminal")
+    terminal.innerHTML=ipm.pc
+}
+function hide(a){
+    let div=document.getElementById(a.id)
+    let nome;
+    for(let i of table){
+        if(i[1]==a.id){
+            nome=i[2];
+        }
+    }
+    let origem =document.getElementById(nome)
+    origem.setAttribute("onclick",`showInfo(${a.id})`)
+    div.style.display='none'
+}
+function showInfo(a){
+    let div=document.getElementById(a.id);
+    let nome;
+    for(let i of table){
+        if(i[1]==a.id){
+            nome=i[2];
+        }
+    }
+    let origem =document.getElementById(nome)
+    origem.setAttribute("onclick",`hide(${a.id})`)
+    div.style.display='inline'
+    
+}
+let tects=0
+let ectsf=0
+function calcTECTS(a,i){
+    let result=0
+    for(let y of a){
+        result+=y.etcs;
+    }
+    let b=document.getElementById(i)
+    b.innerHTML=result
+}
+function calcECTSf(a,i){
+    let result=0
+    for(let y of a){
+        if(y.feita){
+
+            result+=y.etcs;
+        }
+    }
+    let b=document.getElementById(i)
+    b.innerHTML=result
+}
+function getTECTS(a){
+    let result=0
+    for(let y of a){
+        result+=y.etcs;
+    }
+    return result
+}
+function getECTSf(a){
+    let result=0
+    for(let y of a){
+        if(y.feita){
+
+            result+=y.etcs;
+        }
+    }
+   return result
+}
+function calcECTSef(a,i){
+    
+    let e=document.getElementById(i)
+    e.innerHTML=getTECTS(a)-getECTSf(a);
+}
+function calcMed(a,i){
+    let med=0;
+    let etcsd=0
+    let fet=[]
+    for(k of a){
+        if(k.feita){
+            etcsd+=k.etcs
+            fet.push(k);
+        }
+    }
+    for(k of fet ){
+        med+=(k.etcs*k.nota);
+    }
+    med=med/etcsd
+    let medi=document.getElementById(i)
+    medi.innerHTML=med.toFixed(3)
+    if(isNaN(med)){
+        med=0;
+        medi.innerHTML=med.toFixed(0);
+    }
+}
+function initTable(x){
+    getInfo()
+    for(k of table){
+        tects+=k.etcs
+    }
+    let ec = document.getElementById("tects");
+    ec.innerHTML=tects;
+    for(k of apro ){
+        ectsf+=k.etcs
+    }
+    console.log(apro)
+    let ecf = document.getElementById("ectsf");
+    ecf.innerHTML=ectsf;
+    let ecef = document.getElementById("ecef");
+    ecef.innerHTML=tects-ectsf;
+    let info=document.getElementById("inf")
+    info.replaceChildren();
+    for(k of x){
+        let tr=document.createElement('tr');
+        //let div=document.createElement('tr')
+        tr.setAttribute('class',"row")
+        tr.setAttribute('id',k[2])
+        tr.setAttribute("onclick",`showInfo(${k[1]})`)
+        //div.setAttribute("id",k[1])
+        //div.setAttribute("class","rowinfo")
+        //div.setAttribute("colspan","6")
+        let tdSem=document.createElement('td');
+        tdSem.textContent=k.semestre;
+        tdSem.setAttribute("class","tableShowSem");
+        tdSem.setAttribute("onclick",`initTable(cc${k.semestre})`);
+        tr.appendChild(tdSem)
+
+        let tdCodUc=document.createElement('td');
+        tdCodUc.textContent=k.codUC;
+        tr.appendChild(tdCodUc)
+
+        let tdNome=document.createElement('td');
+        tdNome.textContent=k.nome;
+        tr.appendChild(tdNome)
+
+        let tdAc=document.createElement('td');
+        tdAc.textContent=k.aC;
+        tr.appendChild(tdAc)
+
+        let tdetc=document.createElement('td');
+        tdetc.textContent=k.etcs;
+        tr.appendChild(tdetc)
+
+        let tdrel=document.createElement('td');
+        tdrel.textContent=k.feita;
+        tr.appendChild(tdrel)
+
+        let tdfei=document.createElement('td');
+        tdfei.textContent=k.nota;
+        tr.appendChild(tdfei)
+
+        info.appendChild(tr)
+        
+    }
+    
+}
